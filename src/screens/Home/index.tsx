5f3e06c9363391302ff3cdc311ae99bd4d6cd7e4
Bruno Henrique <bruno.sousa@wapstore.com.br>
2021-08-11 17:22:41 -0300

    style: definindo o tema global da aplicação

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,7 +5,7 @@ import * as S from './styles';
 export function Home() {
   return (
     <S.Container>
-
+      <S.Title>Home</S.Title>
     </S.Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,14 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 22px;
+  font-family: ${({ theme }) => theme.fonts.archivo_500};
+  color: ${({ theme }) => theme.colors.title};
+`;
