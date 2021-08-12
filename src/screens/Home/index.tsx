04f9277d016f542e1a3e94aa5020c87339529267
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2021-08-11 23:29:59 -0300

    iniciando a tela de home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { StatusBar } from 'react-native';
+
+import Logo from '../../assets/logo.svg'
 
 import * as S from './styles';
 
 export function Home() {
   return (
     <S.Container>
-      <S.Title>Home</S.Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <S.Header>
+        <S.HeaderWrapper>
+          <Logo
+            width={128}
+            height={12}
+          />
+
+          <S.TotalCars>Total de 12 carros</S.TotalCars>
+        </S.HeaderWrapper>
+      </S.Header>
     </S.Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,14 +1,34 @@
 import styled, { css } from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
-  flex: 1;
-  background-color: ${({ theme }) => theme.colors.background_primary};
-  justify-content: center;
-  align-items: center;
+  ${({ theme }) => css`
+    flex: 1;
+    background-color: ${theme.colors.background_primary};
+  `};
 `;
 
-export const Title = styled.Text`
-  font-size: 22px;
-  font-family: ${({ theme }) => theme.fonts.archivo_500};
-  color: ${({ theme }) => theme.colors.title};
+export const Header = styled.View`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 113px;
+    padding: 28px 18px 24px;
+
+    background-color: ${theme.colors.header};
+  `};
+`;
+
+export const HeaderWrapper = styled.View`
+  height: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+export const TotalCars = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${RFValue(15)}px;
+    color: ${theme.colors.text_detail};
+    font-family: ${theme.fonts.inter_500};
+  `};
 `;
