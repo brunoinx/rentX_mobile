f7b7dc8137e87ef2efed48eb8e9bb00caf6fbfe7
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2021-08-14 11:45:40 -0300

    feat: criando card da lista de carros

---
+++ src/components/Car/styles.ts
@@ -0,0 +1,66 @@
+import styled, { css } from "styled-components/native";
+import { TouchableOpacity } from "react-native";
+
+export const Container = styled(TouchableOpacity)`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 126px;
+    padding: 16px 24px;
+    
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: ${theme.colors.background_secondary};
+  `}
+`;
+
+export const Details = styled.View`
+  height: 100%;
+  width: 40%;
+  justify-content: space-between;
+`;
+
+export const ContentCar = styled.View``
+
+export const ImageCar = styled.Image.attrs({
+  resizeMode: 'stretch'
+})`
+  height: 87px;
+  width: 168px;
+`;
+
+export const WrapperInfoCar = styled.View`
+  margin-bottom: 10px;
+`;
+
+export const Label = styled.Text`
+  ${({ theme }) => css`
+    text-transform: uppercase;
+    font-size: 10px;
+    margin-bottom: 4px;
+    font-family: ${theme.fonts.archivo_500};
+    color: ${theme.colors.text_detail};
+  `}
+`;
+
+export const ModelCar = styled.Text`
+  ${({ theme }) => css`
+    font-size: 15px;
+    font-family: ${theme.fonts.archivo_500};
+    color: ${theme.colors.title};
+  `}
+`;
+
+export const WrapperIcon = styled.View`
+  width: 80%;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+`;
+
+export const PriceText = styled.Text`
+  ${({ theme }) => css`
+    font-size: 15px;
+    font-family: ${theme.fonts.archivo_500};
+    color: ${theme.colors.main};
+  `}
+`;
