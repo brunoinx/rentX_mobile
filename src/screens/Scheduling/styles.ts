f60fb14c26095eddf02a86ca53ef6faf467157fe
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2021-08-27 17:28:36 -0300

    interface de agendamento implmentada

--- src/screens/Scheduling/styles.ts
+++ src/screens/Scheduling/styles.ts
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
+  justify-content: space-between;
 `;
 
 export const Header = styled.View`
@@ -18,9 +19,9 @@ export const Header = styled.View`
 
 export const Title = styled.Text`
   ${({ theme }) => css`
-    line-height: 41px;
-    margin-top: 26px;
-    font-size: ${RFValue(30)}px;
+    line-height: 36px;
+    margin-top: 18px;
+    font-size: ${RFValue(26)}px;
     color: ${theme.colors.text_light};
     font-family: ${theme.fonts.archivo_600};
   `}
@@ -37,9 +38,6 @@ export const RentalPeriod = styled.View`
 export const DateContainer = styled.View`
   ${({ theme }) => css`
     width: 94px;
-    height: 42px;
-
-    flex-direction: row;
     border-bottom-width: 2px;
     border-bottom-color: ${theme.colors.text};
   `}
@@ -47,7 +45,6 @@ export const DateContainer = styled.View`
 
 export const LabelDate = styled.Text`
   ${({ theme }) => css`
-    margin-bottom: 6px;
     font-size: ${RFValue(12)}px;
     color: ${theme.colors.text};
     font-family: ${theme.fonts.archivo_500};
@@ -56,11 +53,17 @@ export const LabelDate = styled.Text`
 
 export const DateInput = styled.TextInput`
   ${({ theme }) => css`
-    height: 50px;
-    width: 70%;
-    margin-left: 15px;
+    height: 30px;
+    width: 100%;
     font-size: ${RFValue(15)}px;
     font-family: ${theme.fonts.inter_500};
     color: ${theme.colors.text_light};
   `}
+`;
+
+export const Content = styled.View`
+`;
+
+export const WrapperButton = styled.View`
+  padding: 10px 20px 16px;
 `;
