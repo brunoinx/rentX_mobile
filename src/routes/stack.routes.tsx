98ce4b3bb9a56e967936cc45fb83491708419fd2
Bruno Henrique <bruno.sousa@wapstore.com.br>
2021-08-29 21:26:50 -0300

    feat: criando rotas da aplicação

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Home } from '../screens/Home';
+import { CarDetails } from '../screens/CarDetails';
+import { Scheduling } from '../screens/Scheduling';
+import { SchedulingDetails } from '../screens/SchedulingDetails';
+import { ScheduleDone } from '../screens/ScheduleDone';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function StackRoutes() {
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Screen name="Home" component={Home} />
+      <Screen name="CarDetails" component={CarDetails} />
+      <Screen name="Scheduling" component={Scheduling} />
+      <Screen name="SchedulingDetails" component={SchedulingDetails} />
+      <Screen name="ScheduleDone" component={ScheduleDone} />
+    </Navigator>
+  );
+}
